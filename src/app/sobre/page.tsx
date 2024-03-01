7c45195dcbe41b90504601183220a5193bde1164
gabriel9silv <134931559+gabriel9silv@users.noreply.github.com>
2024-03-01 11:42:41 -0300

    reset css e link

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Sobre (){
+   return(
+    <>
+        <h1>Sobre</h1>
+        <p>Tela sobre o meu projeto.</p>
+        <Link href={"/"}>Home</Link>
+        
+        </>
+
+   );
+}
